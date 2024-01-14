70e6de966346a90c2f446546272b488125eace4f
ayuuuuayushhhhhh10 <ayushdevda88@gmail.com>
2024-01-14 09:56:26 +0530

    page1 done

--- script.js
+++ script.js
@@ -1,4 +1,5 @@
-var tl = gsap.timeline();
+function loaderAnimation () {
+    var tl = gsap.timeline();
 
 tl.from(".line h1", {
   y: 100,
@@ -26,13 +27,37 @@ tl.from("#line1-part1 ,.line h2", {
 tl.to('#loader',{
     opacity:0,
     duration:0.5,
-    delay:1.5
+    delay:1
 })
 tl.from("#page1",{
     y:1600,
     duration:1,
-    ease:Power4
+    ease:Power4.easeOut
 })
 tl.to('#loader',{
-    display:'none'
+    display:'none',
+    duration:0
 })
+tl.from('#nav',{
+  opacity:0,
+  y:-40
+})
+tl.from('#hero1 h1,#hero2 h1,#hero3 h2,#hero4 h1',{
+  y:120,
+  stagger:0.2
+})
+
+} 
+loaderAnimation();
+
+function cursorAnimation() {
+  document.addEventListener('mousemove',function(e) {
+    gsap.to('#circle',{
+      top:e.y,
+      left:e.x,
+    })
+  })
+  
+  Shery.makeMagnet("#nav-part2 h4");
+}
+cursorAnimation();
